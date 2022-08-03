a1b7d1d28a50a17faf171b4b927257fead1bb6be
Phil Whittington <pwhittin@F2K028.42adel.org.au>
2022-08-03 16:05:26 +0930

    my proper strchr

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,26 +6,21 @@
 /*   By: pwhittin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:01:13 by pwhittin          #+#    #+#             */
-/*   Updated: 2022/08/03 16:02:27 by pwhittin         ###   ########.fr       */
+/*   Updated: 2022/08/03 16:04:53 by pwhittin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_strchr(char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
-	int	i;
+	char	*p;
 
-	i = 0;
-	if (!s)
-		return (0);
-	if (c == '\0')
-		return ((char *)&s[ft_strlen(s)]);
-	while (s[i] != '\0')
+	p = (char *) s;
+	while (*p != (char)c)
 	{
-		if (s[i] == (char) c)
-			return ((char *)&s[i]);
-		i++;
+		if (*p == '\0')
+			return (NULL);
+		p++;
 	}
-	return (0);
 }
